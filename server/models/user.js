f29090bf0f81b2860e49d961c41fd3ec2c2ebdfc
Bohdan <global.site.dev@gmail.com>
2018-10-23 18:23:43 +0300

    Add password hasing
    Using bcrypt to hash passwords before saving a user to the DB

--- server/models/user.js
+++ server/models/user.js
@@ -1,31 +1,54 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     email: {
         type: String,
-        require: true,
+        required: true,
         trim: true,
         unique: 1
     },
     password: {
         type: String,
-        require: true,
-        minlenght: 6,
+        required: true,
+        minlength: 6
     },
     name: {
         type: String,
-        maxlenght: 100,
+        maxlength: 100
     },
     lastname: {
         type: String,
-        maxlenght: 100,
+        maxlength: 100
     },
     role: {
         type: Number,
-        default: 0,
+        default: 0
     },
     token: {
         type: String
+    }
+})
+
+// Hashing password before saving a user to the DB
+
+userSchema.pre('save', function(next) {
+    let user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
     }
 })
 
