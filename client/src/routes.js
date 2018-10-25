5ea71ddbe467df4ec5eeb068499bd6e16209aaab
Bohdan <global.site.dev@gmail.com>
2018-10-25 14:29:15 +0300

    Add basic routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-const routes = () => (
-    
-)
+import Layout from './hoc/Layout';
+import Home from './components/Home/Home';
+import LoginForm from './containers/LoginForm/LoginForm';
+import UserProfile from './containers/UserProfile/UserProfile';
+
+const routes = () => {
+    return (
+        <Layout>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/login" exact component={LoginForm} />
+                <Route path="profile" exact component={UserProfile} />
+            </Switch>
+        </Layout>
+    );
+}
 
 export default routes;
