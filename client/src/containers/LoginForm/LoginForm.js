93a919c6ee6a294b622da7aea1deebc69e14aa81
Bohdan <global.site.dev@gmail.com>
2018-10-29 14:42:02 +0200

    Create login form template

--- client/src/containers/LoginForm/LoginForm.js
+++ client/src/containers/LoginForm/LoginForm.js
@@ -1,20 +1,76 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions';
+import styles from './LoginForm.module.css';
+import Input from '../../components/UI/Input/Input';
 
 class LoginForm extends Component {
+
+    state = {
+        loginForm: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Enter your email'
+                },
+                value: ''
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Enter your password'
+                },
+                value: ''
+            }
+        },
+        error: '',
+        success: false
+    }
 
     submitForm = () => {
 
     }
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.submitForm}>
-                    <h2>LOG IN</h2>
+    inputChangedHandler = (event, inputIdentifier) => {
+        // Clone deeply with spread operator
 
-                    <div></div>
+        const updatedLoginForm = {
+            ...this.state.loginForm
+        };
+        const updatedFormElement = {
+            ...updatedLoginForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedLoginForm[inputIdentifier] = updatedFormElement;
+        this.setState({loginForm: updatedLoginForm});
+    }
+
+    render() {
+        const formElements = [];
+
+        for (let key in this.state.loginForm) {
+            formElements.push({
+                id: key,
+                config: this.state.loginForm[key]
+            });
+        }
+
+
+        return (
+            <div className={styles.Container}>
+                <h2>LOG IN</h2>
+                <form onSubmit={this.submitForm}>
+                    {formElements.map(formElement => (
+                        <Input
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                            />
+                    ))}
                 </form>
             </div>
         )
