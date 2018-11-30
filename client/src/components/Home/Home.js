cea54d31d0a6e5460971ea3cdd8398dee3f1d345
GlobalSiteDev <34938442+GlobalSiteDev@users.noreply.github.com>
2018-11-30 22:35:32 +0200

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,7 +6,7 @@ import styles from './Home.module.css';
 const home = () => (
     <div className={styles.Container}>
         <div className={styles.Content}>
-            <h3>Welcome to my test asignment!</h3>
+            <h3>Welcome to my app!</h3>
             <p>To enter your profile</p>
             <Link to="/login" className={styles.LoginLink}>LOG IN</Link>
             <p>or</p>
